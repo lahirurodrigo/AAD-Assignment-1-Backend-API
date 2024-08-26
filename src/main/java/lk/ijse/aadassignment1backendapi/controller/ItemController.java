8215b17993f6cc783a11b5de547d80ba2d9d84c5
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-08-26 15:48:02 +0530

    create item servelet

---
+++ src/main/java/lk/ijse/aadassignment1backendapi/controller/ItemController.java
@@ -0,0 +1,9 @@
+package lk.ijse.aadassignment1backendapi.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/item")
+public class ItemController extends HttpServlet {
+
+}
