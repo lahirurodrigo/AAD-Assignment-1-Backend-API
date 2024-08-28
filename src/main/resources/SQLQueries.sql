06e0b07dbdf70d420454b538d1c29135ea03a061
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-08-28 19:50:54 +0530

    refactored the sql file

--- src/main/resources/SQLQueries.sql
+++ src/main/resources/SQLQueries.sql
@@ -1,5 +1,5 @@
 create table customer(
-                         id int(10)primary key,
+                         id varchar(12)primary key,
                          name varchar(100)not null,
                          address varchar(100)not null,
                          email varchar(100)not null
@@ -14,7 +14,7 @@ create table item(
 
 create table orders(
                        id varchar(100)primary key,
-                       cusId int(10)not null,
+                       cusId varchar(12)not null,
                        date date not null,
                        foreign key(cusId)references customer(id)
                            on update cascade on delete cascade
