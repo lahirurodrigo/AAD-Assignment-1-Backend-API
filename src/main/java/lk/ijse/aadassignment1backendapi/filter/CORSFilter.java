e5c0194e43a63863ce0652834dc2917ec069ff9f
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-08-28 22:04:49 +0530

    Add CORS filter for servlets

---
+++ src/main/java/lk/ijse/aadassignment1backendapi/filter/CORSFilter.java
@@ -0,0 +1,31 @@
+package lk.ijse.aadassignment1backendapi.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CORSFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("Origin");
+        String configedOrigin = getServletContext().getInitParameter("origin");
+
+        if (origin != null && (configedOrigin.equals("*") || origin.equals(configedOrigin))) {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
+        chain.doFilter(req, res);
+    }
+}
