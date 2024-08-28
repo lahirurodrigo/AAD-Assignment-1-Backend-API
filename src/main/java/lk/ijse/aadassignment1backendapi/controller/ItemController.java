363ea51531bf2d8badacd1fef3b8f6d751d04272
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-08-28 11:29:29 +0530

    mplemented CRUD operation methods in item servlet.

--- src/main/java/lk/ijse/aadassignment1backendapi/controller/ItemController.java
+++ src/main/java/lk/ijse/aadassignment1backendapi/controller/ItemController.java
@@ -1,9 +1,61 @@
 package lk.ijse.aadassignment1backendapi.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/item")
 public class ItemController extends HttpServlet {
+    Connection connection;
+    @Override
+    public void init() throws ServletException {
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/pathumpossystem");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle CREATE operation
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle READ operation
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle UPDATE operation
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle DELETE operation
+    }
+
+
+    @Override
+    public void destroy() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
